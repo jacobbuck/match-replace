902c5c5b67b0f8939ab38cab769b741f59919342
Jacob Buck <buck.jacob@gmail.com>
2017-04-22 15:16:12 +1200

    Tweaks to replacer function

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ module.exports = function matchReplace(str, regexp, replacement) {
   if (!matches) return false;
   return replacement.replace(paramRegexp, function(m, p1) {
     if (p1 === '$') return p1;
-    var i = p1 === '&' ? 0 : parseInt(p1);
-    return i < matches.length ? matches[i] : m;
+    if (p1 === '&') return matches[0];
+    var i = parseInt(p1, 10);
+    if (i > 0 && i < matches.length) return matches[i];
+    return m;
   });
 };
