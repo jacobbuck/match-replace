8dd3c7036549af2a699a0f593f8957d06e34256a
Jacob Buck <buck.jacob@gmail.com>
2017-04-25 15:22:16 +1200

    Fix up $` and $' special replacement patterns

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var paramRegExp = /\$([\$&`']|\d{1,2})/g;
 module.exports = function matchReplace(str, regexp, replacement) {
   var matches = (str || '').match(regexp);
   if (!matches) return false;
-  return replacement.replace(paramRegExp, function(m, p1, i) {
+  return replacement.replace(paramRegExp, function(m, p1) {
     if (p1 === '$') return p1;
     if (p1 === '&') return matches[0];
-    if (p1 === '`') return str.substr(0, i);
-    if (p1 === '\'') return str.substr(i + p1.length);
+    if (p1 === '`') return str.substr(0, matches.index);
+    if (p1 === '\'') return str.substr(matches.index + matches[0].length);
     var i = parseInt(p1, 10);
     if (i > 0 && i < matches.length) return matches[i];
     return m;
