20e5df90f4cdbfb13af73b340cdb625695e6198b
Jacob Buck <buck.jacob@gmail.com>
2017-04-25 14:43:39 +1200

    Add more special replacement patterns based off String.prototype.replace(). Fix a bug where `$n` wasn't working for single digit integers

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
 
-var paramRegExp = /\$(\$|&|\d{2})/g;
+var paramRegExp = /\$([\$&`']|\d{1,2})/g;
 
 module.exports = function matchReplace(str, regexp, replacement) {
   var matches = (str || '').match(regexp);
   if (!matches) return false;
-  return replacement.replace(paramRegexp, function(m, p1) {
+  return replacement.replace(paramRegexp, function(m, p1, i) {
     if (p1 === '$') return p1;
     if (p1 === '&') return matches[0];
+    if (p1 === '`') return str.substr(0, i);
+    if (p1 === '\'') return str.substr(i + p1.length);
     var i = parseInt(p1, 10);
     if (i > 0 && i < matches.length) return matches[i];
     return m;
